97cb68bc528e403ffa69fed565a707b7c455a02a
alchemistmatt <matthew.monroe@pnnl.gov>
2022-05-11 18:15:23 -0700

    Add classes for tracking model config table data

---
+++ FormFieldChooserInfo.cs
@@ -0,0 +1,19 @@
+﻿namespace DMSModelConfigDbUpdater
+{
+    internal class FormFieldChooserInfo : BasicFormField
+    {
+        public string CrossReference { get; }
+
+        public FormFieldChooserInfo(int id, string formFieldName, string crossReference) : base(id, formFieldName)
+        {
+            CrossReference = crossReference;
+        }
+
+        public override string ToString()
+        {
+            return string.IsNullOrWhiteSpace(CrossReference)
+                ? base.ToString()
+                : string.Format("{0} (cross reference {1})", FormFieldName, CrossReference);
+        }
+    }
+}
---
+++ StoredProcArgumentInfo.cs
@@ -0,0 +1,17 @@
+﻿namespace DMSModelConfigDbUpdater
+{
+    internal class StoredProcArgumentInfo : BasicFormField
+    {
+        public string ArgumentName { get; }
+
+        public StoredProcArgumentInfo(int id, string formFieldName, string argumentName) : base(id, formFieldName)
+        {
+            ArgumentName = argumentName;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0}: {1}", FormFieldName, ArgumentName);
+        }
+    }
+}
