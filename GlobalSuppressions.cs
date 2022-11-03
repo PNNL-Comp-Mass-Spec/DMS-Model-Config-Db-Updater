dbe0d34520992b45f72456995ab8cf02335c58fd
alchemistmatt <matthew.monroe@pnnl.gov>
2022-11-02 17:03:29 -0700

    Add dictionary words and suppress warnings

--- GlobalSuppressions.cs
+++ GlobalSuppressions.cs
@@ -5,5 +5,6 @@
 
 using System.Diagnostics.CodeAnalysis;
 
-[assembly: SuppressMessage("Roslynator", "RCS1123:Add parentheses when necessary.", Justification = "Parentheses not needed", Scope = "member", Target = "~M:DMSModelConfigDbUpdater.ModelConfigDbUpdater.UpdateHotlinks(DMSModelConfigDbUpdater.GeneralParameters.ParameterType,System.String,System.String,System.Collections.Generic.List{DMSModelConfigDbUpdater.HotLinkInfo})")]
+[assembly: SuppressMessage("Roslynator", "RCS1123:Add parentheses when necessary.", Justification = "Parentheses not needed", Scope = "member", Target = "~M:DMSModelConfigDbUpdater.ModelConfigDbUpdater.UpdateHotlinksCheckWhichArg(DMSModelConfigDbUpdater.GeneralParameters.ParameterType,System.String,System.String,DMSModelConfigDbUpdater.HotlinkInfo,System.Collections.Generic.IDictionary{System.String,System.Boolean})")]
+[assembly: SuppressMessage("Roslynator", "RCS1123:Add parentheses when necessary.", Justification = "Parentheses not needed", Scope = "member", Target = "~M:DMSModelConfigDbUpdater.ModelConfigDbUpdater.UpdateListReportHotlinks(System.String,System.Collections.Generic.List{DMSModelConfigDbUpdater.HotlinkInfo})")]
 [assembly: SuppressMessage("Style", "IDE0066:Convert switch statement to expression", Justification = "Leave as-is for readability", Scope = "member", Target = "~M:DMSModelConfigDbUpdater.ModelConfigDbUpdater.ColumnRenamed(DMSModelConfigDbUpdater.GeneralParameters.ParameterType,System.String,System.String,System.String@,System.Boolean)~System.Boolean")]
