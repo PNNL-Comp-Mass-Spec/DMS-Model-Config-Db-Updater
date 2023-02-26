ec797582b25a4cd149d6a43b1af83db8f7508cc3
Bryson Gibbons <bryson.gibbons@pnnl.gov>
2023-02-25 23:27:27 -0800

    Update stored procedure names used by the website and their output column names

--- StoredProcColumnNames.cs
+++ StoredProcColumnNames.cs
@@ -22,47 +22,47 @@ namespace DMSModelConfigDbUpdater
         {
             columnNames = storedProcedureName switch
             {
-                "GetDatasetStatsByCampaign" => new List<string>
+                "get_dataset_stats_by_campaign" => new List<string>
                 {
-                    "Campaign",
-                    "Work Package",
-                    "Pct EMSL Funded",
-                    "Runtime Hours",
-                    "Datasets",
-                    "Building",
-                    "Instrument",
-                    "Request Min",
-                    "Request Max",
-                    "Pct Total Runtime"
+                    "campaign",
+                    "work_package",
+                    "pct_emsl_funded",
+                    "runtime_hours",
+                    "datasets",
+                    "building",
+                    "instrument",
+                    "request_min",
+                    "request_max",
+                    "pct_total_runtime"
                 },
-                "GetPackageDatasetJobToolCrosstab" => new List<string>
+                "get_package_dataset_job_tool_crosstab" => new List<string>
                 {
                     "dataset",
                     "jobs",
                     "id"
                 },
-                "FindExistingJobsForRequest" => new List<string>
+                "find_existing_jobs_for_request" => new List<string>
                 {
-                    "Job",
-                    "State",
-                    "Priority",
-                    "Request",
-                    "Created",
-                    "Start",
-                    "Finish",
-                    "Processor",
-                    "Dataset"
+                    "job",
+                    "state",
+                    "priority",
+                    "request",
+                    "created",
+                    "start",
+                    "finish",
+                    "processor",
+                    "dataset"
                 },
-                "FindMatchingDatasetsForJobRequest" => new List<string>
+                "find_matching_datasets_for_job_request" => new List<string>
                 {
-                    "Sel",
-                    "Dataset",
-                    "Jobs",
-                    "New",
-                    "Busy",
-                    "Complete",
-                    "Failed",
-                    "Holding"
+                    "sel",
+                    "dataset",
+                    "jobs",
+                    "new",
+                    "busy",
+                    "complete",
+                    "failed",
+                    "holding"
                 },
                 "GetCurrentMangerActivity" => new List<string>
                 {
@@ -72,27 +72,27 @@ namespace DMSModelConfigDbUpdater
                     "What",
                     "alert"
                 },
-                "PredefinedAnalysisDatasets" => new List<string>
+                "predefined_analysis_datasets" => new List<string>
                 {
-                    "Dataset",
-                    "ID",
-                    "InstrumentClass",
-                    "Instrument",
-                    "Campaign",
-                    "Experiment",
-                    "Organism",
-                    "Exp Labelling",
-                    "Exp Comment",
-                    "DS Comment",
-                    "DS Type",
-                    "DS Rating",
-                    "Rating",
-                    "Sep Type",
-                    "Tool",
-                    "Parameter File",
-                    "Settings File",
-                    "Protein Collections",
-                    "Legacy FASTA"
+                    "dataset",
+                    "id",
+                    "instrument_class",
+                    "instrument",
+                    "campaign",
+                    "experiment",
+                    "organism",
+                    "exp_labelling",
+                    "exp_comment",
+                    "ds_comment",
+                    "ds_type",
+                    "ds_rating",
+                    "rating",
+                    "sep_type",
+                    "tool",
+                    "parameter_file",
+                    "settings_file",
+                    "protein_collections",
+                    "legacy_fasta"
                 },
                 "EvaluatePredefinedAnalysisRules" => new List<string>  // Obsolete stored procedure
                 {
@@ -231,72 +231,72 @@ namespace DMSModelConfigDbUpdater
                     "owner",
                     "export_mode"
                 },
-                "ReportProductionStats" => new List<string>
+                "report_production_stats" => new List<string>
                 {
-                    "Instrument",
-                    "Total Datasets",
-                    "Days in range",
-                    "Datasets per day",
-                    "Blank Datasets",
-                    "QC Datasets",
-                    "Bad Datasets",
-                    "Study Specific Datasets",
-                    "Study Specific Datasets per day",
-                    "EMSL-Funded Study Specific Datasets",
-                    "EF Study Specific Datasets per day",
-                    "Total AcqTimeDays",
-                    "Study Specific AcqTimeDays",
-                    "EF Total AcqTimeDays",
-                    "EF Study Specific AcqTimeDays",
-                    "Hours AcqTime per Day",
-                    "Inst.",
-                    "% Inst EMSL Owned",
-                    "EF Total Datasets",
-                    "EF Datasets per day",
-                    "% Blank Datasets",
-                    "% QC Datasets",
-                    "% Bad Datasets",
-                    "% Study Specific Datasets",
-                    "% EF Study Specific Datasets",
-                    "% EF Study Specific by AcqTime",
-                    "Inst"
+                    "instrument",
+                    "total_datasets",
+                    "days_in_range",
+                    "datasets_per_day",
+                    "blank_datasets",
+                    "qc_datasets",
+                    "bad_datasets",
+                    "study_specific_datasets",
+                    "study_specific_datasets_per_day",
+                    "emsl_funded_study_specific_datasets",
+                    "ef_study_specific_datasets_per_day",
+                    "total_acq_time_days",
+                    "study_specific_acq_time_days",
+                    "ef_total_acq_time_days",
+                    "ef_study_specific_acq_time_days",
+                    "hours_acq_time_per_day",
+                    "inst_",
+                    "pct_inst_emsl_owned",
+                    "ef_total_datasets",
+                    "ef_datasets_per_day",
+                    "pct_blank_datasets",
+                    "pct_qc_datasets",
+                    "pct_bad_datasets",
+                    "pct_study_specific_datasets",
+                    "pct_ef_study_specific_datasets",
+                    "pct_ef_study_specific_by_acq_time",
+                    "inst"
                 },
-                "GetProteinCollectionMemberDetail" => new List<string>
+                "get_protein_collection_member_detail" => new List<string>
                 {
-                    "Protein_Collection_ID",
-                    "Protein_Name",
-                    "Description",
-                    "Protein_Sequence",
-                    "Monoisotopic_Mass",
-                    "Average_Mass",
-                    "Residue_Count",
-                    "Molecular_Formula",
-                    "Protein_ID",
-                    "Reference_ID",
-                    "SHA1_Hash",
-                    "Member_ID",
-                    "Sorting_Index"
+                    "protein_collection_id",
+                    "protein_name",
+                    "description",
+                    "protein_sequence",
+                    "monoisotopic_mass",
+                    "average_mass",
+                    "residue_count",
+                    "molecular_formula",
+                    "protein_id",
+                    "reference_id",
+                    "sha1_hash",
+                    "member_id",
+                    "sorting_index"
                 },
-                "GetFactorCrosstabByBatch" => new List<string>
+                "get_factor_crosstab_by_batch" => new List<string>
                 {
-                    "Sel",
-                    "BatchID",
-                    "Name",
-                    "Status",
-                    "Dataset_ID",
-                    "Request",
-                    "Block",
-                    "Run Order"
+                    "sel",
+                    "batch_id",
+                    "name",
+                    "status",
+                    "dataset_id",
+                    "request",
+                    "block",
+                    "run order"
                 },
-                "GetRequestedRunFactorsForEdit" => new List<string>
+                "get_requested_run_factors_for_edit" => new List<string>
                 {
-                    "Sel",
-                    "BatchID",
-                    "Experiment",
-                    "Dataset",
-                    "Name",
-                    "Status",
-                    "Request"
+                    "sel",
+                    "batch_id",
+                    "experiment",
+                    "dataset",
+                    "name",
+                    "status",
+                    "request"
                 },
                 "ReportRequestDaily" => new List<string>
                 {
@@ -306,45 +306,45 @@ namespace DMSModelConfigDbUpdater
                     "In history list",
                     "Datasets Created"
                 },
-                "ReportTissueUsageStats" => new List<string>
+                "report_tissue_usage_stats" => new List<string>
                 {
-                    "Tissue_ID",
-                    "Tissue",
-                    "Experiments",
-                    "Exp_Created_Min",
-                    "Exp_Created_Max",
-                    "Organism_First",
-                    "Organism_Last",
-                    "Campaign_First",
-                    "Campaign_Last"
+                    "tissue_id",
+                    "tissue",
+                    "experiments",
+                    "datasets",
+                    "instruments",
+                    "instrument_first",
+                    "instrument_last",
+                    "dataset_acq_time_min",
+                    "dataset_acq_time_max",
+                    "organism_first",
+                    "organism_last",
+                    "campaign_first",
+                    "campaign_last",
+                    "exp_created_min",
+                    "exp_created_max"
                 },
-                "GetMonthlyInstrumentUsageReport" => new List<string>
+                "get_monthly_instrument_usage_report" => new List<string>
                 {
-                    "Instrument",
-                    "EMSL_Inst_ID",
-                    "Start",
-                    "Type",
-                    "Minutes",
-                    "Proposal",
-                    "Usage",
-                    "Users",
-                    "Operator",
-                    "Comment",
-                    "Year",
-                    "Month",
-                    "Dataset_ID",
-                    "Start",
-                    "Type",
-                    "Minutes",
-                    "Proposal",
-                    "Usage",
-                    "Comment",
-                    "Dataset_ID",
-                    "Type",
-                    "Minutes",
-                    "Percentage",
-                    "Usage",
-                    "Proposal"
+                    "instrument",
+                    "emsl_inst_id",
+                    "start",
+                    "type",
+                    "minutes",
+                    "proposal",
+                    "usage",
+                    "users",
+                    "operator",
+                    "comment",
+                    "year",
+                    "month",
+                    "dataset_id",
+                    "percentage",
+                    "available",
+                    "duration",
+                    "interval",
+                    "total",
+                    "percentage"
                 },
                 _ => new List<string>()
             };
